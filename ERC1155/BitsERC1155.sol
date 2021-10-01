50e87a9102c5c61d9a5f92d07baf1daa31710c50
rafasantama <rafasantama@gmail.com>
2021-10-01 13:17:22 -0500

    cambios token ERC1155 nativo y marketplace

--- ERC1155/BitsERC1155.sol
+++ ERC1155/BitsERC1155.sol
@@ -3,11 +3,12 @@ pragma solidity 0.8.0;
 import "./ERC1155.sol";
 
 
-contract PROXFactory is ERC1155 {
+contract BITSFactory is ERC1155 {
 
-    constructor(string memory _Url, string) ERC1155(_Url) public {
+    constructor(string memory _uri, bytes memory _data, uint _native_token_supply) ERC1155(_uri) public {
         owner=msg.sender;
         address2state[msg.sender]=true;
+        _mint(owner, 0, _native_token_supply, _data);
     }
 
     // declare our event here
@@ -18,25 +19,25 @@ contract PROXFactory is ERC1155 {
     address owner;
     bool set_state=false;
     uint fee=10;
+    uint id=0;
     
-    struct PRX {
+    struct BIT {
         string name;
-        uint dna;
-        uint attCode;
+        bytes data;
     }
-    PRX[] public PROXs;
+    BIT[] public BITS;
     
-    struct partner{
+    struct publisher{
         string name;
         string ID;
         address partner_address;
     }
     
-    partner[] public partners;
+    publisher[] public publishers;
     
     mapping (address => mapping( string => bool)) created_name;
     mapping (address => bool) address2state;
-    mapping (uint => uint) PRXID2price;
+    mapping (uint => uint) BITSID2price;
     mapping (address => uint) balance;
 
     
@@ -57,39 +58,41 @@ contract PROXFactory is ERC1155 {
         balance[_reciever]+=_amount;
     }
 
-    function add_partner_owner(string memory _name, string memory _ID, address _partner_address) public {
+    function add_publisher_owner(string memory _name, string memory _ID, address _partner_address) public {
         require(msg.sender==owner, "debes ser el owner");
-        partners.push(partner(_name, _ID, _partner_address));
-        address2state[msg.sender]=true;
+        publishers.push(publisher(_name, _ID, _partner_address));
+        address2state[_partner_address]=true;
     }
     // function partner_register( string memory _name, string memory _ID) public{
     //     partners.push(partner(_name, _ID, msg.sender));
     //     address2state[msg.sender]=true;
     // }
 
-    function _createPRX(string memory _name, uint _dna, uint _attCode, uint _price) private {
-        uint _id=PROXs.push(PRX(_name, _dna,_attCode));
-        PRXID2price[_id]=_price;
+    function _createBITS(string memory _name, bytes memory _data, uint _price) private {
+        id++;
+        BITS.push(BIT(_name,_data));
+        BITSID2price[id]=_price;
         // and fire it here
         created_name[msg.sender][_name]=true;
-        _mint(contract_address, _id);
+        _mint(contract_address, id, 1, _data);
     }
 
-    function _generateRandomDna(string memory _str) private view returns (uint) {
-        uint rand = uint(keccak256(abi.encodePacked(_str)))+uint(keccak256(abi.encodePacked(msg.sender)))+uint(keccak256(abi.encodePacked(block.difficulty, now)));
-        return rand % dnaModulus;
-    }
+    //function _generateRandomDna(string memory _str) private view returns (uint) {
+    //    uint rand = uint(keccak256(abi.encodePacked(_str)))+uint(keccak256(abi.encodePacked(msg.sender)))+uint(keccak256(abi.encodePacked(block.difficulty, now)));
+    //    return rand % dnaModulus;
+    //}
 
-    function createUniquePRX(string memory _name, uint _attCode, uint _price) public {
+    function createUniqueBITS(string memory _name, bytes memory _data, uint _price) public {
         require(_price>=0,"el precio debe ser positivo");
-        require(set_state==false,"El contrato ya fue configurado");
+        require(set_state==true,"El contrato debe estar configurado");
         require(created_name[msg.sender][_name]==false, "You cannot create the same repeated name with the same address");
         require(address2state[msg.sender]==true,"debes estar registrarte como partner");
-        uint randDna = _generateRandomDna(_name);
-        _createPRX(_name, randDna, _attCode, _price);
+        //uint randDna = _generateRandomDna(_name);
+        _createBITS(_name, _data, _price);
     }
-    function buy_NFT(uint _NFTid) public {
-        require(PRXID2price[_NFTid]<=balance[msg.sender]);
-        transferFrom( contract_address,  msg.sender, _NFTid);
+    function buy_NFT(uint _NFTid, bytes memory _data) public {
+        require(BITSID2price[_NFTid]<=balanceOf(msg.sender,0));
+        safeTransferFrom(contract_address,  msg.sender, _NFTid, 1 , _data);
+        safeTransferFrom(msg.sender,  msg.sender, 0, BITSID2price[_NFTid], _data);
     }
 }
